b4f365e7a37f1b5901065f93e8f8d82d2abd1de2
Jens Nolte <git@queezle.net>
2021-09-05 22:06:32 +0200

    Add more output to example

--- example/Main.hs
+++ example/Main.hs
@@ -2,10 +2,15 @@ module Main (main) where
 
 import Quasar
 import Quasar.Prelude
+import Quasar.Timer
 import Quasar.Wayland.Client
 
 main :: IO ()
-main = withResourceManagerM do
-  traceIO "Connecting"
-  client <- connectWaylandClient
-  traceIO "Connected"
+main = do
+  withResourceManagerM do
+    traceIO "Connecting"
+    client <- connectWaylandClient
+    traceIO "Connected"
+    await =<< newDelay 1000000
+    traceIO "Closing"
+  traceIO "Closed"
