ba054db2036d39fb2a71943d063da7d43aff9bd4
Jens Nolte <git@queezle.net>
2021-09-14 18:43:26 +0200

    Dump TH splices to file by default

--- src/Quasar/Wayland/Protocol/Generated.hs
+++ src/Quasar/Wayland/Protocol/Generated.hs
@@ -1,10 +1,12 @@
 {-# OPTIONS_GHC -Wno-missing-export-lists #-}
 {-# OPTIONS_GHC -Wno-unused-imports #-}
---{-# OPTIONS_GHC -ddump-splices #-}
+{-# OPTIONS_GHC -ddump-splices #-}
+{-# OPTIONS_GHC -ddump-to-file #-}
 
 module Quasar.Wayland.Protocol.Generated where
 
 -- Imports are here to improve readability when dumping splices
+import Control.Monad.Catch
 import Data.Binary
 import Quasar.Prelude
 import Quasar.Wayland.Protocol.Core
