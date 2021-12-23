dc28bdac392492c9d1680b38360696ac2475c15a
Jens Nolte <git@queezle.net>
2021-12-23 13:52:39 +0100

    Add helper for mmap

--- src/Quasar/Wayland/Utils/SharedMemory.hs
+++ src/Quasar/Wayland/Utils/SharedMemory.hs
@@ -2,24 +2,28 @@
 {-# LANGUAGE TemplateHaskell #-}
 
 module Quasar.Wayland.Utils.SharedMemory (
-  allocateShmFd,
+  memfdCreate,
+  mmap,
 ) where
 
+import Foreign
+import Foreign.Concurrent qualified as FC
 import Foreign.C.Error
 import Language.C.Inline qualified as C
 import Language.C.Inline.Unsafe qualified as CU
 import Quasar.Prelude
 import Quasar.Wayland.Utils.InlineC
-import System.Posix.Types (COff(..), Fd(Fd))
+import System.Posix.Types (Fd(Fd), COff(..))
 
 C.context ctx
 
 C.verbatim "#define _GNU_SOURCE"
 C.include "<unistd.h>"
+C.include "<stdint.h>"
 C.include "<sys/mman.h>"
 
-allocateShmFd :: COff -> IO Fd
-allocateShmFd size = Fd <$> throwErrnoIfMinus1 "allocateShmFd"
+memfdCreate :: COff -> IO Fd
+memfdCreate size = Fd <$> throwErrnoIfMinus1 "memfd_create/ftruncate"
   [CU.block|
     int {
       int fd = memfd_create("shm", MFD_CLOEXEC | MFD_ALLOW_SEALING);
@@ -34,3 +38,14 @@ allocateShmFd size = Fd <$> throwErrnoIfMinus1 "allocateShmFd"
       return fd;
     }
   |]
+
+mmap :: Fd -> C.CSize -> IO (ForeignPtr Word8)
+mmap (Fd fd) size = do
+  ptr <- fmap intPtrToPtr . throwErrnoIfMinus1 "mmap" $
+    ptrToIntPtr <$> [CU.exp| void*{ mmap(NULL, $(size_t size), PROT_READ | PROT_WRITE, MAP_SHARED, $(int fd), 0) } |]
+  FC.newForeignPtr ptr (munmap ptr size)
+
+munmap :: Ptr a -> C.CSize -> IO ()
+munmap (castPtr -> ptr) size =
+  throwErrnoIfMinus1_ "munmap" $
+    [CU.exp| int { munmap($(void* ptr), $(size_t size)) } |]
