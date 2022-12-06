635e067ad614211019e1871c7c7443ad1ea98fa6
Jens Nolte <git@queezle.net>
2022-12-06 22:41:51 +0100

    Change repository layout to support multiple projects

--- flake.nix
+++ flake.nix
@@ -9,19 +9,29 @@
   };
 
   outputs = { self, nixpkgs, quasar }:
+  with nixpkgs.lib;
   let
-    lib = nixpkgs.lib;
-    systems = lib.platforms.unix;
-    forAllSystems = lib.genAttrs systems;
+    systems = platforms.unix;
+    forAllSystems = genAttrs systems;
+    getHaskellPackages = pkgs: pattern: pipe pkgs.haskell.packages [
+      attrNames
+      (filter (x: !isNull (strings.match pattern x)))
+      (sort (x: y: x>y))
+      (map (x: pkgs.haskell.packages.${x}))
+      head
+    ];
   in {
     packages = forAllSystems (system:
-    let pkgs = import nixpkgs { inherit system; overlays = [
+    let
+      pkgs = import nixpkgs { inherit system; overlays = [
         self.overlays.default
         quasar.overlays.default
       ]; };
+      ghc92 = getHaskellPackages pkgs "ghc92.";
     in rec {
-      default = quasar-wayland;
-      quasar-wayland = pkgs.haskell.packages.ghc924.quasar-wayland;
+      default = quasar-wayland-examples;
+      quasar-wayland = ghc92.quasar-wayland;
+      quasar-wayland-examples = ghc92.quasar-wayland-examples;
     }
     );
 
@@ -29,7 +39,8 @@
       default = final: prev: {
         haskell = prev.haskell // {
           packageOverrides = hfinal: hprev: prev.haskell.packageOverrides hfinal hprev // {
-            quasar-wayland = hfinal.callCabal2nix "quasar-wayland" ./. {};
+            quasar-wayland-examples = hfinal.callCabal2nix "quasar-wayland-examples" ./examples {};
+            quasar-wayland = hfinal.callCabal2nix "quasar-wayland" ./quasar-wayland {};
           };
         };
       };
@@ -37,19 +48,28 @@
       quasar = quasar.overlay;
     };
 
-    devShell = forAllSystems (system:
+    devShells = forAllSystems (system:
       let
-        pkgs = nixpkgs.legacyPackages.${system};
-      in pkgs.mkShell {
-        inputsFrom = [ self.packages.${system}.quasar-wayland.env ];
-        packages = [
-          pkgs.cabal-install
-          pkgs.zsh
-          pkgs.entr
-          pkgs.ghcid
-          pkgs.haskell-language-server
-          pkgs.hlint
-        ];
+        pkgs = import nixpkgs { inherit system; overlays = [
+          self.overlays.default
+          quasar.overlays.default
+        ]; };
+        haskellPackages = getHaskellPackages pkgs "ghc92.";
+      in rec {
+        default = haskellPackages.shellFor {
+          packages = hpkgs: [
+            hpkgs.quasar-wayland
+            hpkgs.quasar-wayland-examples
+          ];
+          nativeBuildInputs = [
+            pkgs.cabal-install
+            pkgs.zsh
+            pkgs.entr
+            pkgs.ghcid
+            pkgs.haskell-language-server
+            pkgs.hlint
+          ];
+        };
       }
     );
   };
