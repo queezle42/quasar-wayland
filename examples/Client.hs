5a40fc97b7c4d7c86055a902c1cd2555510bd122
Jens Nolte <git@queezle.net>
2022-12-11 22:18:24 +0100

    Remove commented code from client example

--- examples/Client.hs
+++ examples/Client.hs
@@ -5,11 +5,7 @@ import Quasar.Prelude
 import Quasar.Timer
 import Quasar.Wayland.Client
 import Quasar.Wayland.Client.JuicyPixels
---import Quasar.Wayland.Client.Surface
 import Quasar.Wayland.Client.XdgShell
---import Quasar.Wayland.Protocol
---import Quasar.Wayland.Protocol.Generated
---import Quasar.Wayland.Shared.DummyWindowManager
 import Quasar.Wayland.Shm
 import Quasar.Wayland.Surface
 
@@ -46,87 +42,6 @@ main = do
 
       await =<< newDelay 1000000
 
-    --join $ atomically do
-
-    --  (surface, wlSurface) <- newClientSurface @ShmBufferBackend client
-    --  (surface2, wlSurface2) <- newClientSurface @ShmBufferBackend client
-
-    --  wlrLayerShell <- bindSingleton @Interface_zwlr_layer_shell_v1 client.registry
-
-    --  configuredVar <- newTVar False
-    --  unmappedVar <- newTVar False
-    --  configuredVar2 <- newTVar False
-    --  unmappedVar2 <- newTVar False
-
-    --  wlrLayerSurface <- wlrLayerShell.get_layer_surface wlSurface Nothing 2 "demo"
-    --  setMessageHandler wlrLayerSurface EventHandler_zwlr_layer_surface_v1 {
-    --    configure = \serial _width _height -> do
-    --        whenM (not <$> readTVar unmappedVar) do
-    --          wlrLayerSurface.ack_configure serial
-    --        writeTVar configuredVar True,
-    --    closed = pure ()
-    --  }
-    --  wlrLayerSurface.set_size 512 512
-    --  wlrLayerSurface.set_anchor 1
-
-    --  wlrLayerSurface2 <- wlrLayerShell.get_layer_surface wlSurface2 Nothing 2 "demo"
-    --  setMessageHandler wlrLayerSurface2 EventHandler_zwlr_layer_surface_v1 {
-    --    configure = \serial _width _height -> do
-    --        whenM (not <$> readTVar unmappedVar2) do
-    --          wlrLayerSurface2.ack_configure serial
-    --        writeTVar configuredVar2 True,
-    --    closed = pure ()
-    --  }
-    --  wlrLayerSurface2.set_size 512 512
-    --  wlrLayerSurface2.set_anchor 2
-
-    --  -- Commit role
-    --  wlSurface.commit
-    --  wlSurface2.commit
-    --  -- Should await first `configure` event
-
-    --  pure do
-    --    buffer <- liftIO $ toImageBuffer (mkImage gradient)
-    --    buffer2 <- liftIO $ toImageBuffer (mkImage solidColor)
-
-    --    atomically do
-    --      check =<< readTVar configuredVar
-    --      check =<< readTVar configuredVar2
-    --      commitSurface surface SurfaceCommit {
-    --        buffer = Just buffer,
-    --        offset = (0, 0),
-    --        bufferDamage = DamageAll
-    --      }
-    --      commitSurface surface2 SurfaceCommit {
-    --        buffer = Just buffer2,
-    --        offset = (0, 0),
-    --        bufferDamage = DamageList [Rectangle 0 0 42 42]
-    --      }
-    --      destroyBuffer buffer
-    --      destroyBuffer buffer2
-
-    --    await =<< newDelay 100000
-    --    traceIO "Waiting 2s"
-    --    await =<< newDelay 2000000
-
-    --    atomically do
-    --      commitSurface surface SurfaceCommit {
-    --        buffer = Nothing,
-    --        offset = (0, 0),
-    --        bufferDamage = DamageList []
-    --      }
-    --      writeTVar unmappedVar True
-
-    --    -- await =<< newDelay 100000
-    --    atomically do
-    --      commitSurface surface2 SurfaceCommit {
-    --        buffer = Nothing,
-    --        offset = (0, 0),
-    --        bufferDamage = DamageList []
-    --      }
-    --      writeTVar unmappedVar2 True
-
-    await =<< newDelay 1000000
     traceIO "Closing"
   traceIO "Closed"
 
@@ -153,7 +68,7 @@ mkDimensions :: Integral a => a -> a -> Dimensions
 mkDimensions width height = Dimensions { width = fromIntegral width, height = fromIntegral height, aspect }
   where
     aspect :: Double
-    aspect = (fromIntegral width) / (fromIntegral height)
+    aspect = fromIntegral width / fromIntegral height
 
 mkPosition :: Dimensions -> Int -> Int -> Position
 mkPosition dimensions pixelX pixelY = Position { dimensions, pixelX, pixelY, u, v, x, y }
@@ -161,21 +76,21 @@ mkPosition dimensions pixelX pixelY = Position { dimensions, pixelX, pixelY, u, 
     width' = dimensions.width
     height' = dimensions.height
     u :: Double
-    u = (fromIntegral pixelX) / (fromIntegral width')
+    u = fromIntegral pixelX / fromIntegral width'
     v :: Double
-    v = (fromIntegral pixelY) / (fromIntegral height')
+    v = fromIntegral pixelY / fromIntegral height'
     innerRadius :: Int
     innerRadius = div (min width' height') 2
     x :: Double
-    x = (fromIntegral $ pixelX - (div width' 2)) / (fromIntegral innerRadius)
+    x = fromIntegral (pixelX - (width' `div` 2)) / fromIntegral innerRadius
     y :: Double
-    y = (fromIntegral $ pixelY - (div height' 2)) / (fromIntegral innerRadius)
+    y = fromIntegral (pixelY - (height' `div` 2)) / fromIntegral innerRadius
 
 color :: forall a. RealFrac a => a -> a -> a -> PixelRGBA8
 color r g b = PixelRGBA8 (toWord r) (toWord g) (toWord b) 255
   where
     toWord :: a -> Word8
-    toWord = truncate . (* 255) . (max 0) . (min 1)
+    toWord = truncate . (* 255) . max 0 . min 1
 
 gradient :: Position -> PixelRGBA8
 gradient p = color (u p) 0 (v p)
