1d55b9cf15c8c04f3306ea68dbffef833ff1c294
Jens Nolte <git@queezle.net>
2024-06-08 05:46:57 +0200

    Add basic client wrapper for wl_seat

---
+++ quasar-wayland/src/Quasar/Wayland/Client/Seat.hs
@@ -0,0 +1,44 @@
+module Quasar.Wayland.Client.Seat (
+  Seat(..),
+  getSeats,
+) where
+
+import Quasar.Future
+import Quasar.Observable.Core
+import Quasar.Observable.List (ObservableList)
+import Quasar.Observable.List qualified as ObservableList
+import Quasar.Prelude
+import Quasar.Wayland.Client
+import Quasar.Wayland.Client.Registry
+import Quasar.Wayland.Protocol
+import Quasar.Wayland.Protocol.Generated
+
+data Seat = Seat {
+  wlSeat :: Object 'Client Interface_wl_seat,
+  name :: Future '[] String
+}
+
+getSeats :: WaylandClient -> STMc NoRetry '[] (ObservableList NoLoad '[] Seat)
+getSeats client = getClientComponent (newSeatList client) client
+
+newSeatList :: WaylandClient -> STMc NoRetry '[] (ObservableList NoLoad '[] Seat)
+newSeatList client = ObservableList.cache (traverseGlobals client.registry 9 initializeSeat finalizeSeat)
+
+initializeSeat ::
+  NewObject 'Client Interface_wl_seat ->
+  STMc NoRetry '[] Seat
+initializeSeat wlSeat = do
+  nameVar <- newPromise
+
+  setEventHandler wlSeat EventHandler_wl_seat {
+    capabilities = \_ -> pure (),
+    name = \name -> tryFulfillPromise_ nameVar (toString name)
+  }
+
+  pure Seat {
+    wlSeat,
+    name = toFuture nameVar
+  }
+
+finalizeSeat :: Seat -> STMc NoRetry '[] ()
+finalizeSeat seat = tryCall seat.wlSeat.release
