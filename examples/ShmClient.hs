55ff53f3efee4bca6034b5b97f335c3aff6e833c
Jens Nolte <git@queezle.net>
2024-07-27 00:45:14 +0200

    Update shm example to show more test patterns

--- examples/ShmClient.hs
+++ examples/ShmClient.hs
@@ -32,7 +32,7 @@ main = do
       newWindow windowManager properties (writeTMVar configurationVar) \case
         WindowRequestClose -> writeTVar closeRequestedVar True
 
-    forM_ [solidColor, gradient, gradient2, gradient3, gradient4] \img -> do
+    forM_ [gradient, red, green, blue, alpha, transparent, gradient, gradient2, gradient3, gradient4] \img -> do
       -- Blocks until first configure event
       configuration <- atomically $ readTMVar configurationVar
       let width = max configuration.width 512
@@ -90,26 +90,44 @@ mkPosition dimensions pixelX pixelY = Position { dimensions, pixelX, pixelY, u, 
     y :: Double
     y = fromIntegral (pixelY - (height' `div` 2)) / fromIntegral innerRadius
 
-color :: forall a. RealFrac a => a -> a -> a -> PixelRGBA8
-color r g b = PixelRGBA8 (toWord r) (toWord g) (toWord b) 255
+rgb :: Double -> Double -> Double -> PixelRGBA8
+rgb r g b = PixelRGBA8 (toWord r) (toWord g) (toWord b) 0
   where
-    toWord :: a -> Word8
+    toWord :: Double -> Word8
+    toWord = truncate . (* 255) . max 0 . min 1
+
+rgba :: Double -> Double -> Double -> Double -> PixelRGBA8
+rgba r g b a = PixelRGBA8 (toWord r) (toWord g) (toWord b) (toWord a)
+  where
+    toWord :: Double -> Word8
     toWord = truncate . (* 255) . max 0 . min 1
 
 gradient :: Position -> PixelRGBA8
-gradient p = color (u p) 0 (v p)
+gradient p = rgb (u p) 0 (v p)
 
 gradient2 :: Position -> PixelRGBA8
-gradient2 p = color (v p) 0 (1 - u p)
+gradient2 p = rgb (v p) 0 (1 - u p)
 
 gradient3 :: Position -> PixelRGBA8
-gradient3 p = color (1 - u p) 0 (1 - v p)
+gradient3 p = rgb (1 - u p) 0 (1 - v p)
 
 gradient4 :: Position -> PixelRGBA8
-gradient4 p = color (1 - v p) 0 (u p)
+gradient4 p = rgb (1 - v p) 0 (u p)
 
-solidColor :: Position -> PixelRGBA8
-solidColor _p = color (255 :: Double) 0 0
+red :: Position -> PixelRGBA8
+red = const (rgb 1 0 0)
+
+green :: Position -> PixelRGBA8
+green = const (rgb 0 1 0)
+
+blue :: Position -> PixelRGBA8
+blue = const (rgb 0 0 1)
+
+alpha :: Position -> PixelRGBA8
+alpha p = rgba 1 0 1 (v p)
+
+transparent :: Position -> PixelRGBA8
+transparent p = rgba 0 0 0 0
 
 mkImage :: Int32 -> Int32 -> (Position -> PixelRGBA8) -> JuicyPixels.Image PixelRGBA8
 mkImage width height fn = generateImage pixel (fromIntegral width) (fromIntegral height)
