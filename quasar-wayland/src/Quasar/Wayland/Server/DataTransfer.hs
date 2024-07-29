72a12691aba91670776c1731647dcf0d20cb5765
Jens Nolte <git@queezle.net>
2024-07-29 17:10:46 +0200

    Stub get_data_device

--- quasar-wayland/src/Quasar/Wayland/Server/DataTransfer.hs
+++ quasar-wayland/src/Quasar/Wayland/Server/DataTransfer.hs
@@ -14,5 +14,16 @@ initializeDataDeviceManager :: NewObject 'Server Interface_wl_data_device_manage
 initializeDataDeviceManager wlDataDeviceManager = do
   wlDataDeviceManager `setRequestHandler` RequestHandler_wl_data_device_manager {
     create_data_source = undefined,
-    get_data_device = undefined
+    get_data_device = initializeDataDevice
+  }
+
+initializeDataDevice ::
+  NewObject 'Server Interface_wl_data_device ->
+  Object 'Server Interface_wl_seat ->
+  STMc NoRetry '[SomeException] ()
+initializeDataDevice wlDataDevice wlSeat = do
+  wlDataDevice `setRequestHandler` RequestHandler_wl_data_device {
+    start_drag = undefined,
+    set_selection = undefined,
+    release = pure ()
   }
